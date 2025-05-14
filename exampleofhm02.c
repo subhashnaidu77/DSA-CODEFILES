cacdfb33d8fe920878ce6a413cfc8a7ba4f32579
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2025-05-14 23:41:41 +0530

    simple

---
+++ exampleofhm02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	char str[100];
+	int a , b,c;
+	c=scanf("%d %d",&a,&b);
+	printf("%d",c);
+	// int var=scanf("%s",str);
+	// printf("%d",var);
+	return 0;
+}
